8c21ae1acec76f7bf5b665a23bd21b5771c96331
huanght <hht51770@hotmail.com>
2018-06-03 15:54:20 +0800

    添加立即执行函数

--- js/loading.js
+++ js/loading.js
@@ -1,9 +1,10 @@
-loading();
+!function () {
+    loading.call();
 
-
-/****************加载动画********************************/
-function loading() {
-    setTimeout(function () {
-        upload.classList.remove('active');
-    },1000);
-}
+    /****************加载动画********************************/
+    function loading() {
+        setTimeout(function () {
+            upload.classList.remove('active');
+        },1000);
+    }
+}.call();
--- js/smoothly-navigation.js
+++ js/smoothly-navigation.js
@@ -1,59 +1,51 @@
+!function () {
+    linstenMenu.call();
+    linstenNavA.call();
 
-linstenMenu();
-linstenNavA();
-
-function linstenMenu() {
-    let liTags = document.querySelectorAll('nav.menu > ul > li');
-    for(let i = 0; i < liTags.length; i++){
-        liTags[i].onmouseenter = function (e) {
-            e.currentTarget.classList.add('active');
-        }
-        liTags[i].onmouseleave = function (e) {
-            e.currentTarget.classList.remove('active');
+    function linstenMenu() {
+        let liTags = document.querySelectorAll('nav.menu > ul > li');
+        for(let i = 0; i < liTags.length; i++){
+            liTags[i].onmouseenter = function (e) {
+                e.currentTarget.classList.add('active');
+            };
+            liTags[i].onmouseleave = function (e) {
+                e.currentTarget.classList.remove('active');
+            }
         }
     }
-}
 
-function linstenNavA() {
-    let aTags = document.querySelectorAll('nav.menu > ul > li > a');
-    for(let i = 0; i < aTags.length; i++){
-        aTags[i].onclick = function (e) {
-            e.preventDefault();
-            let a = e.currentTarget;
-            let href = a.getAttribute('href');
-            let element = document.querySelector(href);
-            let targetTop = element.offsetTop - 80;
-            // let n = 20;
-            // let t =  500 / n;
-            let currentTop = window.scrollY;
-            let s = (Math.abs(targetTop - currentTop))/ 100;
-            // id = setInterval( ()=> {           //生硬的动画
-            //     if(n-- < 0){
-            //         window.clearInterval(id);
-            //         return;
-            //     }
-            //     i = i + 1;
-            //     window.scrollTo(0, currentTop + s * i);
-            // },t);
-            /* 缓动*/
-            function animate(time) {
+    function linstenNavA() {
+        let aTags = document.querySelectorAll('nav.menu > ul > li > a');
+        for(let i = 0; i < aTags.length; i++){
+            aTags[i].onclick = function (e) {
+                e.preventDefault();
+                let a = e.currentTarget;
+                let href = a.getAttribute('href');
+                let element = document.querySelector(href);
+                let targetTop = element.offsetTop - 80;
+                let currentTop = window.scrollY;
+                let s = (Math.abs(targetTop - currentTop))/ 100;
+                /* 缓动  tween插件代码*/
+                function animate(time) {
+                    requestAnimationFrame(animate);
+                    TWEEN.update(time);
+                }
                 requestAnimationFrame(animate);
-                TWEEN.update(time);
+                let t = s * 300;
+                if(t > 900){
+                    t = 900;
+                }
+                var coords = { y: currentTop }; // Start at (0, 0)
+                var tween = new TWEEN.Tween(coords)
+                    .to({ y : targetTop }, t)
+                    .easing(TWEEN.Easing.Quadratic.InOut)
+                    .onUpdate(function() {
+                        window.scrollTo(0 , coords.y);
+                    })
+                    .start();
             }
-            requestAnimationFrame(animate);
-            let t = s * 300;
-            if(t > 900){
-                t = 900;
-            }
-            var coords = { y: currentTop }; // Start at (0, 0)
-            var tween = new TWEEN.Tween(coords)
-                .to({ y : targetTop }, t)
-                .easing(TWEEN.Easing.Quadratic.InOut)
-                .onUpdate(function() {
-                    window.scrollTo(0 , coords.y);
-                })
-                .start();
         }
     }
-}
+}.call();
+
 
